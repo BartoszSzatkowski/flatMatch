c49d7be4cf9fb6c93d77a52f2e313764068a78fc
BartoszSzatkowski <szatek24@gmail.com>
2021-11-10 15:31:31 +0100

    feat: add basic apollo server set up

---
+++ server/src/index.js
@@ -0,0 +1,22 @@
+'use strict';
+const { sequelize } = require('./model/index');
+const { ApolloServer, gql } = require('apollo-server');
+const { readFileSync } = require('fs');
+const path = require('path');
+const resolvers = require('./graphql/resolvers');
+
+const types = readFileSync(
+  path.join(__dirname, 'graphql', 'types.graphql'),
+  'utf8'
+);
+
+const typeDefs = gql`
+  ${types}
+`;
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+(async () => {
+  await sequelize.sync();
+  server.listen().then(({ url }) => console.log(`server running on ${url}`));
+})();
--- server/src/model/user.js
+++ server/src/model/user.js
@@ -1,33 +1,33 @@
 'use strict';
 
-function createUser (sequelize, DataTypes) {
-    const User = sequelize.define('User', {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false;
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-            },
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        factors: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-    });
-    return User;
+function createUser(sequelize, DataTypes) {
+  const User = sequelize.define('User', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    factors: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  });
+  return User;
 }
 
 module.exports = createUser;
