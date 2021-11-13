9941702cdcf45a5636ef6427825ee8a6127fea7a
BartoszSzatkowski <szatek24@gmail.com>
2021-11-13 17:49:51 +0100

    style: add authorisation stack final styling

--- client/components/authStack/Login.jsx
+++ client/components/authStack/Login.jsx
@@ -1,22 +1,43 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import InputBox from '../UI/InputBox';
+import StyledText from '../UI/StyledText';
+import Title from '../UI/Title';
 
 export default function Login({ navigation }) {
   return (
-    <View>
-      <Text>Welcome to the login page</Text>
-      <Button
-        title='Register'
-        color='black'
-        onPress={() => navigation.navigate('Register')}
-      />
-      <Button
-        title='Login'
-        color='black'
-        onPress={() => navigation.navigate('SetupStack')}
-      />
-    </View>
+    <SafeAreaView>
+      <View style={styles.body}>
+        <Title>{'function login() {'}</Title>
+        <StyledText>{'  if(registered) {'}</StyledText>
+        <InputBox
+          fields={['login', 'password']}
+          isMulti={false}
+          isNum={false}
+        />
+        <Button
+          title='Login'
+          color='black'
+          onPress={() => navigation.navigate('SetupStack')}
+        />
+        <StyledText>{'  } else {'}</StyledText>
+        <Button
+          title='Register'
+          color='black'
+          onPress={() => navigation.navigate('Register')}
+        />
+        <StyledText>{'  }'}</StyledText>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  body: {
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'space-around',
+    padding: 30,
+  },
+});
--- client/components/authStack/Register.jsx
+++ client/components/authStack/Register.jsx
@@ -1,22 +1,42 @@
 import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import InputBox from '../UI/InputBox';
+import StyledText from '../UI/StyledText';
+import Title from '../UI/Title';
 
 export default function Register({ navigation }) {
   return (
-    <View>
-      <Text>Welcome To the Register</Text>
-      <Button
-        title='Register'
-        color='black'
-        onPress={() => navigation.navigate('SetupStack')}
-      />
-      <Button
-        title='Go back to login'
-        color='black'
-        onPress={() => navigation.navigate('Login')}
-      />
-    </View>
+    <SafeAreaView>
+      <View style={styles.body}>
+        <Title>{'try {'}</Title>
+        <InputBox
+          fields={['email', 'password', 'repeat password']}
+          isMulit={false}
+          inNum={false}
+        />
+        <Button
+          title='Register'
+          color='black'
+          onPress={() => navigation.navigate('SetupStack')}
+        />
+        <StyledText>{'  } catch (error = "Go back!") {'}</StyledText>
+        <Button
+          title='Go back to login'
+          color='black'
+          onPress={() => navigation.navigate('Login')}
+        />
+        <StyledText>{'  }'}</StyledText>
+      </View>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  body: {
+    display: 'flex',
+    height: '100%',
+    justifyContent: 'space-around',
+    padding: 30,
+  },
+});
