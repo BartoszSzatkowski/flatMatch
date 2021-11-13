9a0e309f10b416c270417783b9d6ed701406c094
BartoszSzatkowski <szatek24@gmail.com>
2021-11-13 16:24:44 +0100

    style: add coords screen styling

--- client/components/setupStack/Coords.jsx
+++ client/components/setupStack/Coords.jsx
@@ -1,13 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
+import Title from '../UI/Title';
+import StyledText from '../UI/StyledText';
 
-export default function Coords() {
+export default function Coords({ navigation }) {
+  const [marker, setMarker] = useState({});
+
+  const putMarker = (coordinates) => {
+    setMarker({
+      latitude: coordinates.latitude,
+      longitude: coordinates.longitude,
+    });
+  };
   return (
     <SafeAreaView>
-      <View style={styles.cointainer}>
-        <MapView style={styles.map} />
+      <View style={{ padding: 30 }}>
+        <Title>@where?</Title>
+        <StyledText>Max distance from ideal location</StyledText>
+        <View style={styles.inputCointainer}>
+          <TextInput
+            style={styles.input}
+            placeholder='(km)'
+            keyboardType='numeric'
+          />
+        </View>
+        <StyledText>Mark your ideal location:</StyledText>
+        <View style={styles.cointainer}>
+          <MapView
+            initialRegion={{
+              latitude: 45.78825,
+              longitude: 15.4324,
+              latitudeDelta: 50,
+              longitudeDelta: 50,
+            }}
+            style={styles.map}
+            onPress={(event) => putMarker(event.nativeEvent.coordinate)}
+          >
+            {Object.keys(marker).length !== 0 && <Marker coordinate={marker} />}
+          </MapView>
+        </View>
+        <Button
+          title='Finish >'
+          color='black'
+          onPress={() => navigation.navigate('HomeTabs')}
+        />
       </View>
     </SafeAreaView>
   );
@@ -16,12 +54,30 @@ export default function Coords() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
   },
   map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
+    width: 330,
+    height: 430,
+  },
+  inputCointainer: {
+    backgroundColor: 'black',
+    paddingBottom: 10,
+    marginBottom: 5,
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '97%',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderTopColor: 'black',
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
   },
 });
