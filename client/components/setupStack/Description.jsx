38762f8ca50e48d13b3c66f331df0249a64daece
BartoszSzatkowski <szatek24@gmail.com>
2021-11-13 11:32:22 +0100

    style: add description screen styling

--- client/components/setupStack/Description.jsx
+++ client/components/setupStack/Description.jsx
@@ -1,17 +1,62 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import StyledText from '../UI/StyledText';
+import Title from '../UI/Title';
 
 export default function Description({ navigation }) {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Banana desc</Text>
+      <View
+        style={{
+          display: 'flex',
+          justifyContent: 'space-around',
+          height: '100%',
+          padding: 30,
+        }}
+      >
+        <Title>Hello world!</Title>
+        <View style={styles.inputCointainer}>
+          <TextInput
+            multiline
+            style={styles.input}
+            placeholder='Write short text about yourself char(255)'
+          />
+        </View>
+        <StyledText>Three important things about me:</StyledText>
+        <View style={styles.inputCointainer}>
+          <TextInput style={styles.input} placeholder='point nr0' />
+          <TextInput style={styles.input} placeholder='point nr1' />
+          <TextInput style={styles.input} placeholder='point nr2' />
+        </View>
+        <Button
+          title='NEXT >'
+          style={styles.button}
+          color='black'
+          onPress={() => navigation.navigate('Coords')}
+        />
       </View>
-      <Button
-        title='Yo it is a string'
-        onPress={() => navigation.navigate('Coords')}
-      />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  inputCointainer: {
+    backgroundColor: 'black',
+    paddingBottom: 10,
+  },
+  input: {
+    backgroundColor: 'white',
+    width: '97%',
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingTop: 5,
+    paddingBottom: 5,
+    borderTopColor: 'black',
+    borderTopWidth: 1,
+    borderLeftWidth: 1,
+  },
+  button: {
+    color: 'black',
+  },
+});
